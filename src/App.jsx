d44689c4585a95176869447157953792d267bf52
lawanaji <lawanmustaphaaji2015@gmail.com>
2024-11-07 01:54:32 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
         {/* Protected Routes */}
